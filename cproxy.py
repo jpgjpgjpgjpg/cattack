4f106786b6d1ac05fd90bcd27a48a118324d4dd6
jpg <163206473+jpgjpgjpgjpg@users.noreply.github.com>
2024-07-22 15:09:17 -0300

    cproxy.py

--- cproxy.py
+++ cproxy.py
@@ -1,44 +1,31 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import requests
 import random
 import time
+from bs4 import BeautifulSoup
 
-def req(url, proxies):
+def generate_headers():
     user_agents = [
-    # Chrome on Windows
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36',
-    # Chrome on macOS
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36',
-    # Chrome on Linux
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
-    # Chrome on Android
-    'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36',
-
-    # Edge on Windows
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.48',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36 Edg/91.0.864.37',
-    # Edge on macOS
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.48',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36 Edg/91.0.864.37',
-    # Edge on Android
-    'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36 EdgA/46.6.2.5140',
-
-    # Firefox on Windows
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
-    # Firefox on macOS
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0',
-    # Firefox on Linux
-    'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
-    'Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
-    # Firefox on Android
-    'Mozilla/5.0 (Android 11; Mobile; rv:89.0) Gecko/89.0 Firefox/89.0',
-    'Mozilla/5.0 (Android 11; Mobile; rv:88.0) Gecko/88.0 Firefox/88.0'
-]
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.48',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36 Edg/91.0.864.37',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.48',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36 Edg/91.0.864.37',
+        'Mozilla/5.0 (Linux; Android 11; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36 EdgA/46.6.2.5140',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0',
+        'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
+        'Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+        'Mozilla/5.0 (Android 11; Mobile; rv:89.0) Gecko/89.0 Firefox/89.0',
+        'Mozilla/5.0 (Android 11; Mobile; rv:88.0) Gecko/88.0 Firefox/88.0'
+    ]
     accept_languages = [
         'en-US,en;q=0.5',
         'en-US,en;q=0.9,es;q=0.8',
@@ -61,23 +48,39 @@ def req(url, proxies):
         'https://www.bing.co.uk/',  
         'https://www.bing.com.br/',
     ]
-
-    headers = {
+    
+    return {
         'User-Agent': random.choice(user_agents),
         'Accept-Language': random.choice(accept_languages),
         'Connection': 'keep-alive',
         'Referer': random.choice(referers),
     }
     
+def req(proxies,url):
+    headers = generate_headers()
     proxy = random.choice(proxies)
+    response = requests.get(url, proxies=proxy, headers=headers)
+    return response, headers
 
-    response = requests.get(url, headers=headers, proxies=proxy)
-    print(f'Código de resposta para {url}: {response.status_code}')
-    print(response.text)
+def ua_response(proxies, url_ua):
+    response, _ = req(proxies, url_ua)
+    if response:
+        print(f"Código de resposta: {response.status_code}")
+        soup = BeautifulSoup(response.text, "html.parser")
+        ua_element = soup.find("textarea", {"id": "custom-ua-string"})
+        ua = ua_element.text.strip()
+        print(f'\nUser-Agent detectado: {ua}')
+    
+def ip_response(proxies, url_padrao):
+    response, _ = req(proxies, url_padrao)
+    if response:
+        print(f'\nCódigo de resposta: {response.status_code}')
+        print(f'IP detectado: {response.text}')
 
 def main():
-    url = "http://httpbin.org/ip"
-    #url = "https://prostactive.online/"
+
+    url_ip = "http://httpbin.org/ip"  # Checar IP
+    url_ua = "https://www.whatsmyua.info/"  # Checar UA
 
     proxies = [
         {'http': 'socks5h://127.0.0.1:9050', 'https': 'socks5h://127.0.0.1:9050'},
@@ -87,13 +90,47 @@ def main():
         {'http': 'socks5h://127.0.0.1:9450', 'https': 'socks5h://127.0.0.1:9450'}
     ]
 
-    intervals = [2,4,5,7]
-    num_requests = [1, 3, 5, 7, 10]
+    print("Escolha a opção:")
+    print("1 - Iniciar ataque")
+    print("2 - Checagem")
+    choice = input("Digite sua escolha (1 ou 2): ")
 
-    for interval, num_req in zip(intervals, num_requests):
-        for _ in range(num_req):
-            req(url, proxies)
-        time.sleep(interval)
+    if choice == '1':
+        intervals = [2, 4, 5, 7,]
+        num_requests = [1, 3, 5, 7, 10]
 
+        url_padrao = input("Digte a URL desejada (Ex: https://google.com/): ")
+
+        for interval, num_req in zip(intervals, num_requests):
+            for _ in range(num_req):
+                response, headers = req(proxies, url_padrao)
+                print(f'\nURL da página: {response.url}')
+                print(f'Código de resposta: {response.status_code}')
+                print(f'User-Agent utilizado: {headers["User-Agent"]}')
+                time.sleep(interval)
+
+    elif choice == '2':
+        print(f"\nEscolha a checagem:")
+        print("1 - Checar IP")
+        print("2 - Checar User-Agent")
+        print("3 - Checar Headers")
+        sub_choice = input("Digite sua escolha (1, 2 ou 3): ")
+
+        if sub_choice == "1":
+            print("Iniciando checagem de IP...")
+            for _ in range(8):
+                ip_response(proxies, url_ip)
+
+        elif sub_choice == "2":
+            print("Iniciando checagem de User-Agent...")
+            for _ in range(5):
+                ua_response(proxies, url_ua)
+            
+        elif sub_choice == "3":
+            print("Iniciando checagem de Headers...")
+            for _ in range(5):
+                response, headers = req(proxies, url_ip)
+                print(f'\nCódigo de resposta: {response.status_code}')
+                print(f'Headers utilizados: {headers}')
 
 main()
